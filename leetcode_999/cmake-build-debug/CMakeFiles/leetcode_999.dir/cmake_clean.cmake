87fb5a6cbc1f5b43aecedc564b7210279dd2c59e
zpx <1257630257@qq.com>
2020-05-02 20:27:05 +0800

    可以被一步捕获的棋子数

---
+++ leetcode_999/cmake-build-debug/CMakeFiles/leetcode_999.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leetcode_999.dir/test.c.o"
+  "leetcode_999"
+  "leetcode_999.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/leetcode_999.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
