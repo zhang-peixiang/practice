2842ee737f4dff70a3cc4d27163d63ed4f00d0d1
zhangpeixiang <1257630257@qq.com>
2020-05-17 00:17:24 +0800

    种花问题

--- leetcode_605/leetcode_605/test.c
+++ leetcode_605/leetcode_605/test.c
@@ -6,11 +6,22 @@
 //
 //给定一个花坛（表示为一个数组包含0和1，其中0表示没种植花，1表示种植了花），和一个数 n 。
 //能否在不打破种植规则的情况下种入 n 朵花？能则返回True，不能则返回False。
+
+/*
+分两种情况讨论：
+1.长度小于3的情况
+(1)当长度小于3时，只要中间有一个1且n不为0时，返回false，n为0时，返回true
+(2)全为0时([0,0,0])，最多只能种一个，所以n小于等于1时，返回true，否则返回false。
+2.长度大于等于3的情况
+(1)当第一个(i=0)为0和第二个为0的时候，可以在第一个位置上种花。
+(2)当最后一个位置上为0并且倒数第二个位置上为0时，可以在最后一个位置上种花
+(3)中间位置上，三个连续的0可以种一个花，然后i+2避免重复计算(例如：[1,0,0,0,0,1])
+*/
 bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n)
 {
 	int i = 0;
 	int count = 0;
-	if (flowerbedSize < 3)
+	if (flowerbedSize < 3)//判断长度为3以下的情况
 	{
 		for (i = 0; i < flowerbedSize; i++)
 		{
@@ -23,7 +34,7 @@ bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n)
 				return false;
 			}
 		}
-		if (n < 2&&f lowerbedSize!=0)
+		if (n < 2&& flowerbedSize!=0)
 		{
 			return true;
 		}
@@ -36,7 +47,7 @@ bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n)
 			if (flowerbed[i] == 0 && flowerbed[i + 1] == 0)
 			{
 				count++;
-				i += 1;
+				i += 1;//避免重复计算，i+=2，因为循环结束会加1，所以这里加1  例如[0,0,0,1]
 			}
 		}
 		else if (flowerbed[i] == 0 && flowerbed[i - 1] == 0 && i == flowerbedSize - 1)
