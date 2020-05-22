fc14fbc2fe28beb0bc2e7d1d51f1d9687a0fdb06
zhangpeixiang <1257630257@qq.com>
2020-05-23 01:07:30 +0800

    合并两个有序数组

--- leetcode_88/leetcode_88/test.c
+++ leetcode_88/leetcode_88/test.c
@@ -1,32 +1,47 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-
+#include <assert.h>
 //给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
 
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n)
 {
 	assert(nums1 &&nums2);
-	int i = 0;
-	int count1 = 0;
-	int count2 = 0;
+	int count1 = m-1;
+	int count2 = n-1;
 	int count = m + n - 1;
-	while (count>=0)
+	while (count1>=0 && count2>=0)
 	{
-		if (nums1[m - 1] > nums2[n - 1])
+		if (nums1[count1] > nums2[count2])
 		{
-			nums1[count--] = nums1[m - 1];
-			m--;
+			nums1[count] = nums1[count1];
+			count--;
+			count1--;
 		}
 		else
 		{
-			nums1[count--] = nums2[n - 1];
-			n--;
+			nums1[count] = nums2[count2];
+			count--;
+			count2--;
 		}
+	}
+	while (count1>=0)
+	{
+		nums1[count] = nums1[count1];
+		count--;
+		count1--;
+	}
+	while (count2>=0)
+	{
+		nums1[count] = nums2[count2];
+		count--;
+		count2--;
 	}
 }
 
 int main()
 {
-
+	int nums1[] = { 1, 2, 3, 0, 0, 0 };
+	int nums2[] = { 2, 5, 6 };
+	merge(nums1, 6, 3, nums2, 3, 3);
 	return 0;
 }
