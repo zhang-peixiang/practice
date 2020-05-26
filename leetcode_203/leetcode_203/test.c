68f5d2864d933c9650b6725fe8146d6da935c225
zhangpeixiang <1257630257@qq.com>
2020-05-26 23:27:36 +0800

    移除链表元素

--- leetcode_203/leetcode_203/test.c
+++ leetcode_203/leetcode_203/test.c
@@ -1,1 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//删除链表中等于给定值 val 的所有节点。
+
+struct ListNode
+{
+	int val;
+	struct ListNode* next;
+};
+
+struct ListNode* removeElements(struct ListNode* head, int val)
+{
+	struct ListNode* cur = head;
+	struct ListNode* prev = NULL;
+	while (cur)
+	{
+		if (cur->val == val)
+		{
+			if (cur == head)
+			{
+				//是头节点--头删
+				prev = cur->next;
+				head = prev;
+				free(cur);
+				cur = head;
+			}
+			else
+			{
+				prev->next = cur->next;
+				free(cur);
+				cur = prev->next;
+			}
+		}
+		else
+		{
+			prev = cur;
+			cur = cur->next;
+		}
+		
+	}
+	return head;
+}
