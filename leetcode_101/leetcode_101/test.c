074c06656963851f3428968ff218f39aa5940b21
zhangpeixiang <1257630257@qq.com>
2020-07-17 01:14:46 +0800

    对称二叉树

---
+++ leetcode_101/leetcode_101/test.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdbool.h>
+
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+};
+
+bool _isSymmetric(struct TreeNode* left, struct TreeNode* right)
+{
+	if (left == NULL && right == NULL)
+		return true;
+	if (left == NULL || right == NULL)
+		return false;
+
+	if (left->val != right->val)
+		return false;
+	return _isSymmetric(left->left, right->right) && _isSymmetric(left->right, right->left);
+}
+
+bool isSymmetric(struct TreeNode* root)
+{
+	if (root == NULL)
+		return true;
+	return _isSymmetric(root->left, root->right);
+}
