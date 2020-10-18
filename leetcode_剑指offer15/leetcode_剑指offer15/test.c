2ac573782dd10328f701504e6e37c2be3754864f
zhangpeixiang <1257630257@qq.com>
2020-10-18 17:12:18 +0800

    二进制中1的个数

---
+++ leetcode_剑指offer15/leetcode_剑指offer15/test.c
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+// 请实现一个函数，输入一个整数，输出该数二进制表示中 1 的个数。
+// 例如，把 9 表示成二进制是 1001，有 2 位是 1。因此，如果输入 9，则该函数输出 2。
+
+
+int hammingWeight(int n)
+{
+	int count = 0;
+	while (n)
+	{
+		if (n & 1 == 1)
+		{
+			count++;
+		}
+		n = n >> 1;
+	}
+	return count;
+}
